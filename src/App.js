b7cb08d806b4f72e1febd4c4977f00afb6dc07e6
Neşathan Öztürk <nesathanozturk@Nesathan-MacBook-Pro.local>
2023-01-26 19:26:12 +0300

    feat: added home page in appjs

--- src/App.js
+++ src/App.js
@@ -1,5 +1,12 @@
+import { Routes, Route } from "react-router-dom";
+import Home from "./components/Home/Home";
+
 function App() {
-  return <div className="App"></div>;
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+    </Routes>
+  );
 }
 
 export default App;
