c09c31c09e6be28306f75bb4f301dbfc7cab9539
Neşathan Öztürk <nesathanozturk@Nesathan-MacBook-Pro.local>
2023-01-26 20:20:32 +0300

    style: edited navbar

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,11 +1,13 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
 import "./Navbar.css";
+import shopping from "../assets/shopping.png";
 
 function Navbar() {
   return (
     <header>
       <nav>
+        <img src={shopping} alt="Shopping Cart Logo" />
         <ul>
           <li>
             <NavLink className="link" to="/">
