0a8adb403383a724c680f51af79b16322f5f8bfc
Neşathan Öztürk <nesathanozturk@Nesathan-MacBook-Pro.local>
2023-01-26 20:37:37 +0300

    style: edited card

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,8 +1,33 @@
 import React from "react";
-import "Card.css";
+import "./Card.css";
 
 function Card() {
-  return <div>Card</div>;
+  return (
+    <div class="container">
+      <div class="card">
+        <div class="image">
+          <img src="https://assets.codepen.io/4164355/shoes.png" />
+        </div>
+        <div class="content">
+          <h2>Nike Shoes</h2>
+          <div class="size">
+            <h3>Size :</h3>
+            <span>7</span>
+            <span>8</span>
+            <span>9</span>
+            <span>10</span>
+          </div>
+          <div class="color">
+            <h3>Color :</h3>
+            <span></span>
+            <span></span>
+            <span></span>
+          </div>
+          <a>Add to basket</a>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Card;
