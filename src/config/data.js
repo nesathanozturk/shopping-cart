f353b69c4e9961b1d8e5510439825b92d045166e
Neşathan Öztürk <nesathanozturk@Nesathan-MacBook-Pro.local>
2023-01-26 20:54:46 +0300

    feat: created data

---
+++ src/config/data.js
@@ -0,0 +1,45 @@
+const products = [
+  {
+    id: 1,
+    name: "Nike Air Force",
+    price: 1999.0,
+    image:
+      "https://e7.pngegg.com/pngimages/449/411/png-clipart-air-force-1-sneakers-skate-shoe-nike-nike-white-outdoor-shoe.png",
+  },
+  {
+    id: 2,
+    name: "Chuck 70",
+    price: 2.149,
+    image:
+      "https://w7.pngwing.com/pngs/5/235/png-transparent-chuck-taylor-all-stars-converse-sneakers-motorcycle-shoe-motorcycle-leather-outdoor-shoe-converse-thumbnail.png",
+  },
+  {
+    id: 3,
+    name: "Vans Old Skool",
+    price: 1959.0,
+    image:
+      "https://w7.pngwing.com/pngs/189/485/png-transparent-vans-old-skool-sneakers-shoe-converse-old-skool-white-outdoor-shoe-converse.png",
+  },
+  {
+    id: 4,
+    name: "adidas Superstar",
+    price: 2749.0,
+    image:
+      "https://w7.pngwing.com/pngs/492/876/png-transparent-adidas-superstar-adidas-originals-shoe-sneakers-adidas-white-fashion-outdoor-shoe.png",
+  },
+  {
+    id: 5,
+    name: "Nike Blazer Mid '77",
+    price: 2199.0,
+    image: "https://image.pngaaa.com/142/3672142-middle.png",
+  },
+  {
+    id: 6,
+    name: "New Balance 530",
+    price: 3299.0,
+    image:
+      "https://sneakerbaker.com/wp-content/uploads/2022/10/New-Balance-530-Black-White2.png",
+  },
+];
+
+export default products;
