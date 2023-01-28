cf31c5bde1fba6f9b35af2846bcb6a921c4e506f
Neşathan Öztürk <nesathanozturk@Nesathan-MacBook-Pro.local>
2023-01-28 17:20:38 +0300

    refactor: edited data

--- src/config/data.js
+++ src/config/data.js
@@ -2,93 +2,93 @@ const products = [
   {
     id: 1,
     name: "Nike Air Force",
-    price: 1999.0,
     image:
       "https://www.pngarts.com/files/8/Nike-Air-Force-One-PNG-High-Quality-Image.png",
+    price: 1999.0,
     amount: 1,
   },
   {
     id: 2,
-    name: "Chuck 70",
-    price: 2.149,
+    name: "Converse Chuck 70",
     image: "https://pngimg.com/uploads/converse/small/converse_PNG66.png",
+    price: 2.149,
     amount: 1,
   },
   {
     id: 3,
     name: "Vans Old Skool",
-    price: 1959.0,
     image: "https://pngimg.com/d/vans_PNG22.png",
+    price: 1959.0,
     amount: 1,
   },
   {
     id: 4,
     name: "adidas Superstar",
-    price: 2749.0,
     image:
       "https://www.pngkey.com/png/full/249-2499887_adidas-kids-superstar-grade-school-casual-shoe-transparent.png",
+    price: 2749.0,
     amount: 1,
   },
   {
     id: 5,
     name: "Nike Blazer Mid '77",
-    price: 2199.0,
     image:
       "https://static.nike.com/a/images/t_default/cbeeacf3-2101-4a4d-bdb9-9a689d715ffc/blazer-mid-77-vintage-ayakkab%C4%B1s%C4%B1-CBDjT0.png",
+    price: 2199.0,
     amount: 1,
   },
   {
     id: 6,
     name: "New Balance 530",
-    price: 3299.0,
     image:
       "https://sneakerbaker.com/wp-content/uploads/2022/10/New-Balance-530-Black-White2.png",
+    price: 3299.0,
     amount: 1,
   },
   {
     id: 7,
     name: "Nike Air Force",
-    price: 1999.0,
     image:
       "https://www.pngarts.com/files/8/Nike-Air-Force-One-PNG-High-Quality-Image.png",
+    price: 1999.0,
     amount: 1,
   },
   {
     id: 8,
     name: "Chuck 70",
-    price: 2.149,
     image: "https://pngimg.com/uploads/converse/small/converse_PNG66.png",
+    price: 2.149,
     amount: 1,
   },
   {
     id: 9,
     name: "Vans Old Skool",
-    price: 1959.0,
     image: "https://pngimg.com/d/vans_PNG22.png",
+    price: 1959.0,
     amount: 1,
   },
   {
     id: 10,
     name: "adidas Superstar",
-    price: 2749.0,
     image:
       "https://www.pngkey.com/png/full/249-2499887_adidas-kids-superstar-grade-school-casual-shoe-transparent.png",
+    price: 2749.0,
     amount: 1,
   },
   {
     id: 11,
     name: "Nike Blazer Mid '77",
-    price: 2199.0,
     image:
       "https://static.nike.com/a/images/t_default/cbeeacf3-2101-4a4d-bdb9-9a689d715ffc/blazer-mid-77-vintage-ayakkab%C4%B1s%C4%B1-CBDjT0.png",
+    price: 2199.0,
     amount: 1,
   },
   {
     id: 12,
     name: "New Balance 530",
-    price: 3299.0,
     image:
       "https://sneakerbaker.com/wp-content/uploads/2022/10/New-Balance-530-Black-White2.png",
+    price: 3299.0,
     amount: 1,
   },
 ];
