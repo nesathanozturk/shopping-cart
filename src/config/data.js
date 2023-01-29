d26577e8ea4e48452f874576ace4a0705f803e14
Neşathan Öztürk <nesathanozturk@Nesathan-MacBook-Pro.local>
2023-01-29 22:11:36 +0300

    refactor: edited images

--- src/config/data.js
+++ src/config/data.js
@@ -47,48 +47,41 @@ const products = [
   },
   {
     id: 7,
-    name: "Nike Air Force",
-    image:
-      "https://www.pngarts.com/files/8/Nike-Air-Force-One-PNG-High-Quality-Image.png",
-    price: 1999,
+    name: "Nike Dunk Low Retro 'Panda'",
+    image: "https://static.sneakerjagers.com/products/660x660/185988.jpg",
+    price: 2249,
     amount: 1,
   },
   {
     id: 8,
-    name: "Chuck 70",
-    image: "https://pngimg.com/uploads/converse/small/converse_PNG66.png",
-    price: 2149,
-    amount: 1,
-  },
-  {
-    id: 9,
-    name: "Vans Old Skool",
-    image: "https://pngimg.com/d/vans_PNG22.png",
-    price: 1959,
-    amount: 1,
-  },
-  {
-    id: 10,
-    name: "adidas Superstar",
+    name: "Adidas Stan Smith",
     image:
-      "https://www.pngkey.com/png/full/249-2499887_adidas-kids-superstar-grade-school-casual-shoe-transparent.png",
+      "https://upload.wikimedia.org/wikipedia/commons/0/03/Stan_Smith_white_and_green.png",
     price: 2749,
     amount: 1,
   },
   {
-    id: 11,
-    name: "Nike Blazer Mid '77",
+    id: 9,
+    name: "Puma",
     image:
-      "https://static.nike.com/a/images/t_default/cbeeacf3-2101-4a4d-bdb9-9a689d715ffc/blazer-mid-77-vintage-ayakkab%C4%B1s%C4%B1-CBDjT0.png",
-    price: 2199,
+      "https://pngfile.net/download/Tvg6nUeYNLxxF1q9HjmKYSHa74AKlONM7yOQpfb4B9JhXLMgsf7OFq7xtFvkWu6zvlaBpSDNsBcTh51sYCIAj8L2KpVc0KS515xyafryOts4YYqMyvXJIJTaf15e7GgOJvU5woZ31hnkvFJOSjMk1qsftUEYS1THsbcZrYKBe7GkjQ4rLgtldZliaQ43e318z00LPb2J/medium",
+    price: 1400,
     amount: 1,
   },
   {
-    id: 12,
-    name: "New Balance 530",
+    id: 10,
+    name: "New Balance 990",
     image:
-      "https://sneakerbaker.com/wp-content/uploads/2022/10/New-Balance-530-Black-White2.png",
-    price: 3299,
+      "https://cdn.shopify.com/s/files/1/0324/6781/2487/products/new-balance-990-m990gl3-1_600x600_crop_center.png?v=1603524257",
+    price: 6299,
+    amount: 1,
+  },
+  {
+    id: 11,
+    name: "Nike Air Max",
+    image:
+      "https://cms-cdn.thesolesupplier.co.uk/2017/09/Nike-Lunarglide-6-White2_w900.png",
+    price: 1700,
     amount: 1,
   },
 ];
