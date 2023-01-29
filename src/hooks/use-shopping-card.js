02ca48064c67606554a843305d412156c557f2c4
Neşathan Öztürk <nesathanozturk@Nesathan-MacBook-Pro.local>
2023-01-29 21:26:38 +0300

    refactor: deleted context

--- src/hooks/use-shopping-card.js
+++
@@ -1,8 +0,0 @@
-import { useContext } from "react";
-import ShoppingCard from "../context/ShoppingCardContext";
-
-function useShoppingCardContext() {
-  return useContext(ShoppingCard);
-}
-
-export default useShoppingCardContext;
