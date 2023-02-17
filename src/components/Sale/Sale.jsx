9f4b1637086c58888237aca9ffeff69adda93464
Neşathan Öztürk <nesathanozturk@Nesathan-MacBook-Pro.local>
2023-02-17 17:07:41 +0300

    feat: created sale component

---
+++ src/components/Sale/Sale.jsx
@@ -0,0 +1,5 @@
+function Sale() {
+  return <div></div>;
+}
+
+export default Sale;
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -2,6 +2,7 @@ import Navbar from "../../components/Navbar/Navbar";
 import Preview from "../../components/Preview/Preview";
 import Orders from "../../components/Orders/Orders";
 import NewArrivals from "../../components/NewArrivals/NewArrivals";
+import Sale from "../../components/Sale/Sale";
 import Footer from "../../components/Footer/Footer";
 
 function Home() {
@@ -11,6 +12,7 @@ function Home() {
       <Preview />
       <Orders />
       <NewArrivals />
+      <Sale />
     </div>
   );
 }
