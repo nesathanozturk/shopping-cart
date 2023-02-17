d47735efb8a0a24801545c8a7c1d43d3ddec2343
Neşathan Öztürk <nesathanozturk@Nesathan-MacBook-Pro.local>
2023-02-17 15:05:10 +0300

    style: edited navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,7 +1,36 @@
-import styles from "./styles.jsx";
+import {
+  Nav,
+  NavHeader,
+  Title,
+  NavLinks,
+  Basket,
+  Image,
+  Options,
+} from "./styles.jsx";
+import { BsFillBasketFill } from "react-icons/bs";
 
 function Navbar() {
-  return <div>Navbar</div>;
+  return (
+    <Nav>
+      <NavHeader class="nav-header">
+        <Title class="nav-title">Ozturk.</Title>
+      </NavHeader>
+      <NavLinks class="nav-links">
+        <Basket>
+          <BsFillBasketFill />
+        </Basket>
+        <Image
+          src="https://www.w3schools.com/howto/img_avatar.png"
+          alt="User Profile"
+        />
+        <Options className="options">
+          <span>Basket</span>
+          <span>Account</span>
+          <span>Log out</span>
+        </Options>
+      </NavLinks>
+    </Nav>
+  );
 }
 
 export default Navbar;
---
+++ src/components/Navbar/styles.jsx
@@ -0,0 +1,69 @@
+import styled from "styled-components";
+
+export const Nav = styled.nav`
+  width: 100%;
+  height: 70px;
+  padding: 10px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  position: relative;
+  -webkit-box-shadow: 0px 12px 14px -3px rgba(0, 0, 0, 0.33);
+  -moz-box-shadow: 0px 12px 14px -3px rgba(0, 0, 0, 0.33);
+  box-shadow: 0px 12px 14px -3px rgba(0, 0, 0, 0.33);
+`;
+
+export const NavHeader = styled.div`
+  display: inline;
+`;
+
+export const Title = styled.div`
+  margin: 15px;
+  font-size: 30px;
+  font-weight: 700;
+`;
+
+export const NavLinks = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const Basket = styled.span`
+  background-color: #ccc;
+  color: #fff;
+  margin: 10px 5px 5px 0;
+  border-radius: 50%;
+  padding: 5px 10px 0 10px;
+  font-size: 25px;
+  cursor: pointer;
+`;
+
+export const Options = styled.div`
+  background-color: #ccc;
+  color: #fff;
+  padding: 20px;
+  display: none;
+  border-radius: 10px;
+  cursor: pointer;
+  transition: all 0.5s ease-in-out;
+`;
+
+export const Image = styled.img`
+  width: 44px;
+  height: 44px;
+  margin: 10px 10px 10px 10px;
+  border-radius: 50%;
+  cursor: pointer;
+
+  &:hover ~ ${Options} {
+    position: absolute;
+    top: 80px;
+    right: 5px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+    gap: 10px;
+  }
+`;
