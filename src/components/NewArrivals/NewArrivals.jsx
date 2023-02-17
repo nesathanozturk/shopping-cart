f61f17171b656a8ba4e8a0de83a7e8eda2ef15fb
Neşathan Öztürk <nesathanozturk@Nesathan-MacBook-Pro.local>
2023-02-17 17:05:36 +0300

    style: edited new arrivals

--- src/components/NewArrivals/NewArrivals.jsx
+++ src/components/NewArrivals/NewArrivals.jsx
@@ -1,5 +1,75 @@
+import {
+  Section,
+  Wrapper,
+  Info,
+  Title,
+  Description,
+  Products,
+  ProductItem,
+  CardImg,
+  CardContent,
+  CardTitle,
+  CardPrice,
+} from "./styles.jsx";
+
 function NewArrivals() {
-  return <section className="new-arrivals"></section>;
+  return (
+    <Section>
+      <Wrapper>
+        <Info>
+          <Title>New Arrivals</Title>
+          <Description>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Inventore,
+            fugiat. Cumque ducimus sit quos corrupti.
+          </Description>
+        </Info>
+        <Products>
+          <ProductItem>
+            <div>
+              <CardImg
+                src="https://media.istockphoto.com/id/1255917798/photo/white-marble-product-display-on-gray-background-with-modern-backdrops-studio-empty-pedestal.jpg?s=612x612&w=0&k=20&c=CiyNoKMt9WFOfhJ33KTuhw9WS5RJQAjCvZLu7pDA9A0="
+                alt="Product"
+              />
+              <CardContent>
+                <CardTitle>White Marble</CardTitle>
+              </CardContent>
+              <CardContent>
+                <CardPrice>$300</CardPrice>
+              </CardContent>
+            </div>
+          </ProductItem>
+          <ProductItem>
+            <div>
+              <CardImg
+                src="https://media.istockphoto.com/id/1255917798/photo/white-marble-product-display-on-gray-background-with-modern-backdrops-studio-empty-pedestal.jpg?s=612x612&w=0&k=20&c=CiyNoKMt9WFOfhJ33KTuhw9WS5RJQAjCvZLu7pDA9A0="
+                alt="Product"
+              />
+              <CardContent>
+                <CardTitle>White Marble</CardTitle>
+              </CardContent>
+              <CardContent>
+                <CardPrice>$300</CardPrice>
+              </CardContent>
+            </div>
+          </ProductItem>
+          <ProductItem>
+            <div>
+              <CardImg
+                src="https://media.istockphoto.com/id/1255917798/photo/white-marble-product-display-on-gray-background-with-modern-backdrops-studio-empty-pedestal.jpg?s=612x612&w=0&k=20&c=CiyNoKMt9WFOfhJ33KTuhw9WS5RJQAjCvZLu7pDA9A0="
+                alt="Product"
+              />
+              <CardContent>
+                <CardTitle>White Marble</CardTitle>
+              </CardContent>
+              <CardContent>
+                <CardPrice>$300</CardPrice>
+              </CardContent>
+            </div>
+          </ProductItem>
+        </Products>
+      </Wrapper>
+    </Section>
+  );
 }
 
 export default NewArrivals;
--- src/components/NewArrivals/styles.jsx
+++ src/components/NewArrivals/styles.jsx
@@ -1,1 +1,73 @@
 import styled from "styled-components";
+
+export const Section = styled.section`
+  width: 90%;
+  margin: 50px auto auto auto;
+`;
+
+export const Wrapper = styled.div``;
+
+export const Info = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const Title = styled.h1`
+  margin-bottom: 10px;
+  font-size: 2.5rem;
+  letter-spacing: 2px;
+  text-align: center;
+`;
+
+export const Description = styled.p`
+  color: #666;
+  margin-bottom: 10px;
+  font-size: 1rem;
+  letter-spacing: 1px;
+`;
+
+export const Products = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  gap: 3rem;
+  margin-top: 40px;
+`;
+
+export const ProductItem = styled.div`
+  background-color: #fff;
+  width: 17rem;
+  overflow: hidden;
+  border-radius: 0.4rem;
+  cursor: pointer;
+  box-shadow: 0 3rem 6rem rgba(0, 0, 0, 0.1);
+  transition: 0.2s;
+
+  &:hover {
+    transform: translateY(-0.5%);
+    box-shadow: 0 4rem 8rem rgba(0, 0, 0, 0.5);
+  }
+`;
+
+export const CardImg = styled.img`
+  width: 100%;
+  height: 20rem;
+`;
+
+export const CardContent = styled.div`
+  padding: 0.4rem;
+`;
+
+export const CardTitle = styled.p`
+  font-size: 1.6rem;
+  letter-spacing: 0.1rem;
+  color: #3d3d3d;
+`;
+
+export const CardPrice = styled.p`
+  font-size: 1rem;
+  letter-spacing: 0.1rem;
+  color: #666;
+`;
