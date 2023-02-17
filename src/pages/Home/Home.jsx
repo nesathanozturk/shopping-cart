136528576824e28ac052ed3b36731ac22ae96a50
Neşathan Öztürk <nesathanozturk@Nesathan-MacBook-Pro.local>
2023-02-17 15:34:41 +0300

    feat: created orders components

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,12 +1,14 @@
 import Navbar from "../../components/Navbar/Navbar";
-import Footer from "../../components/Footer/Footer";
 import Preview from "../../components/Preview/Preview";
+import Orders from "../../components/Orders/Orders";
+import Footer from "../../components/Footer/Footer";
 
 function Home() {
   return (
     <div>
       <Navbar />
       <Preview />
+      <Orders />
     </div>
   );
 }
