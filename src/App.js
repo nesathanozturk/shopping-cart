943999483f8102a264411203b2b19cbd11955706
Neşathan Öztürk <nesathanozturk@Nesathan-MacBook-Pro.local>
2023-02-17 13:47:38 +0300

    feat: created routes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
-import React from "react";
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Home/Home";
+import Login from "./pages/Login/Login";
+import NotFound404 from "./pages/NotFound404/NotFound404";
 
 function App() {
-  return <div>App</div>;
+  return (
+    <Routes>
+      <Route path="/login" element={<Login />} />
+      <Route path="/" element={<Home />} />
+      <Route path="*" element={<NotFound404 />} />
+    </Routes>
+  );
 }
 
 export default App;
