0e0f0e2f9c56333bd3944c7bac0e624fa4f1661f
Neşathan Öztürk <nesathanozturk@Nesathan-MacBook-Pro.local>
2023-02-17 17:15:30 +0300

    style: edited sale

--- src/components/Sale/Sale.jsx
+++ src/components/Sale/Sale.jsx
@@ -1,5 +1,74 @@
+import {
+  Section,
+  Wrapper,
+  Info,
+  Title,
+  Products,
+  ProductItem,
+  CardImg,
+  CardContent,
+  CardTitle,
+  CardPrice,
+  CardSale,
+} from "./styles.jsx";
+
 function Sale() {
-  return <div></div>;
+  return (
+    <Section>
+      <Wrapper>
+        <Info>
+          <Title>On Sale Products</Title>
+        </Info>
+        <Products>
+          <ProductItem>
+            <div>
+              <CardImg
+                src="https://media.istockphoto.com/id/1255917798/photo/white-marble-product-display-on-gray-background-with-modern-backdrops-studio-empty-pedestal.jpg?s=612x612&w=0&k=20&c=CiyNoKMt9WFOfhJ33KTuhw9WS5RJQAjCvZLu7pDA9A0="
+                alt="Product"
+              />
+              <CardContent>
+                <CardTitle>White Marble</CardTitle>
+              </CardContent>
+              <CardContent>
+                <CardPrice>$300</CardPrice>
+              </CardContent>
+              <CardSale>Sale!</CardSale>
+            </div>
+          </ProductItem>
+          <ProductItem>
+            <div>
+              <CardImg
+                src="https://media.istockphoto.com/id/1255917798/photo/white-marble-product-display-on-gray-background-with-modern-backdrops-studio-empty-pedestal.jpg?s=612x612&w=0&k=20&c=CiyNoKMt9WFOfhJ33KTuhw9WS5RJQAjCvZLu7pDA9A0="
+                alt="Product"
+              />
+              <CardContent>
+                <CardTitle>White Marble</CardTitle>
+              </CardContent>
+              <CardContent>
+                <CardPrice>$300</CardPrice>
+              </CardContent>
+              <CardSale>Sale!</CardSale>
+            </div>
+          </ProductItem>
+          <ProductItem>
+            <div>
+              <CardImg
+                src="https://media.istockphoto.com/id/1255917798/photo/white-marble-product-display-on-gray-background-with-modern-backdrops-studio-empty-pedestal.jpg?s=612x612&w=0&k=20&c=CiyNoKMt9WFOfhJ33KTuhw9WS5RJQAjCvZLu7pDA9A0="
+                alt="Product"
+              />
+              <CardContent>
+                <CardTitle>White Marble</CardTitle>
+              </CardContent>
+              <CardContent>
+                <CardPrice>$300</CardPrice>
+              </CardContent>
+              <CardSale>Sale!</CardSale>
+            </div>
+          </ProductItem>
+        </Products>
+      </Wrapper>
+    </Section>
+  );
 }
 
 export default Sale;
