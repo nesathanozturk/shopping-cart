4350aa144e7dd4f89f953965a8a0c5858d8cfc1f
Neşathan Öztürk <nesathanozturk@Nesathan-MacBook-Pro.local>
2023-02-18 22:39:57 +0300

    feat: created products

--- src/assets/data.js
+++ src/assets/data.js
@@ -29,3 +29,125 @@ export const images = [
     alt: "Background Poster",
   },
 ];
+
+export const products = [
+  {
+    id: 1,
+    title: "Red Lips",
+    image:
+      "https://images.unsplash.com/photo-1586495777744-4413f21062fa?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=715&q=80",
+    price: 20,
+  },
+  {
+    id: 2,
+    title: "White Watch",
+    image:
+      "https://images.unsplash.com/photo-1523275335684-37898b6baf30?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1399&q=80",
+    price: 50,
+  },
+  {
+    id: 3,
+    title: "Rayban Glass",
+    image:
+      "https://images.unsplash.com/photo-1572635196237-14b3f281503f?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=880&q=80",
+    price: 100,
+  },
+  {
+    id: 4,
+    title: "Black T-Shirt",
+    image:
+      "https://images.unsplash.com/photo-1503341504253-dff4815485f1?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80",
+    price: 30,
+  },
+  {
+    id: 5,
+    title: "Out Cast T-Shirt",
+    image:
+      "https://images.unsplash.com/photo-1527719327859-c6ce80353573?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=764&q=80",
+    price: 40,
+  },
+  {
+    id: 6,
+    title: "Gray Jacket",
+    image:
+      "https://images.unsplash.com/photo-1580742471944-c1c187a943e2?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=668&q=80",
+    price: 60,
+  },
+  {
+    id: 7,
+    title: "Blue Jacket",
+    image:
+      "https://images.unsplash.com/photo-1551537482-f2075a1d41f2?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80",
+    price: 50,
+  },
+  {
+    id: 8,
+    title: "Brown Coat",
+    image:
+      "https://images.unsplash.com/photo-1608063615781-e2ef8c73d114?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80",
+    price: 80,
+  },
+  {
+    id: 9,
+    title: "White T-Shirt",
+    image:
+      "https://images.unsplash.com/photo-1627910087352-f4e3e28d9f5f?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80",
+    price: 20,
+  },
+  {
+    id: 10,
+    title: "Black T-Shirt",
+    image:
+      "https://images.unsplash.com/photo-1627910016961-ee310ef0b108?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=627&q=80",
+    price: 15,
+  },
+  {
+    id: 11,
+    title: "White Shirt",
+    image:
+      "https://images.unsplash.com/photo-1621072156002-e2fccdc0b176?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80",
+    price: 45,
+  },
+  {
+    id: 12,
+    title: "Black Shirt",
+    image:
+      "https://images.unsplash.com/photo-1624824216985-5639b3071ce6?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1yZWxhdGVkfDZ8fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=800&q=60",
+    price: 42,
+  },
+  {
+    id: 13,
+    title: "Suit",
+    image:
+      "https://images.unsplash.com/photo-1619102814948-e164e584cf0c?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80",
+    price: 150,
+  },
+  {
+    id: 14,
+    title: "Chuck 70",
+    image:
+      "https://images.unsplash.com/photo-1463100099107-aa0980c362e6?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80",
+    price: 100,
+  },
+  {
+    id: 15,
+    title: "Adidas",
+    image:
+      "https://images.unsplash.com/flagged/photo-1556637640-2c80d3201be8?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80",
+    price: 120,
+  },
+  {
+    id: 16,
+    title: "Nike",
+    image:
+      "https://images.unsplash.com/photo-1516478177764-9fe5bd7e9717?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80",
+    price: 130,
+  },
+  {
+    id: 17,
+    title: "Blue Suit",
+    image:
+      "https://images.unsplash.com/photo-1593030103066-0093718efeb9?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1160&q=80",
+    price: 180,
+  },
+];
