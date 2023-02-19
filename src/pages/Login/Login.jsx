72d4e1084055c7b567c1eb08ade8a4afada20471
Neşathan Öztürk <nesathanozturk@Nesathan-MacBook-Pro.local>
2023-02-19 14:05:36 +0300

    style: edited login page

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,7 +1,51 @@
-import React from "react";
+import {
+  Section,
+  Container,
+  Wrapper,
+  Form,
+  InputContainer,
+  Title,
+  Help,
+  Input,
+  CheckboxContainer,
+  Label,
+  Link,
+  ButtonContainer,
+  Button,
+} from "./styles.jsx";
+import Footer from "../../components/Footer/Footer";
 
 function Login() {
-  return <div>Login</div>;
+  return (
+    <Section>
+      <Container>
+        <Wrapper>
+          <Title>Log in</Title>
+          <Form>
+            <InputContainer>
+              <Input type="text" placeholder="Username" />
+            </InputContainer>
+            <InputContainer>
+              <Input type="password" placeholder="Password" />
+            </InputContainer>
+            <Help>
+              <CheckboxContainer>
+                <input type="checkbox" name="checkbox" />
+                <Label>Remember me</Label>
+              </CheckboxContainer>
+              <div>
+                <Link href="/">Forgot?</Link>
+              </div>
+            </Help>
+            <ButtonContainer>
+              <Button type="button">Log In</Button>
+            </ButtonContainer>
+          </Form>
+        </Wrapper>
+      </Container>
+      <Footer />
+    </Section>
+  );
 }
 
 export default Login;
