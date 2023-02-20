7b3f1507431c8550d8170e5da1184289d2241d73
Neşathan Öztürk <nesathanozturk@Nesathan-MacBook-Pro.local>
2023-02-20 13:22:48 +0300

    feat: imported auth state

--- src/redux/app/store.js
+++ src/redux/app/store.js
@@ -1,10 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
-import authReducer from "../features/authSlice";
+import authSlice from "../features/authSlice";
 import basketReducer from "../features/basketSlice";
 
 export const store = configureStore({
   reducer: {
     // The section that keeps our states.
+    auth: authSlice.reducer,
     basket: basketReducer,
   },
 });
