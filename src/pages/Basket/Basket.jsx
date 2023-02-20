71004356dd3f044bbc249fa7c27959e8ba4e2ec4
Neşathan Öztürk <nesathanozturk@Nesathan-MacBook-Pro.local>
2023-02-20 12:01:55 +0300

    style: edited basket

--- src/pages/Basket/Basket.jsx
+++ src/pages/Basket/Basket.jsx
@@ -1,67 +1,16 @@
 import Navbar from "../../components/Navbar/Navbar";
 import BasketItem from "../../components/BasketItem/BasketItem";
-import styled from "styled-components";
 import { useSelector, useDispatch } from "react-redux";
 import { basketActions } from "../../redux/features/basketSlice";
-import { RiErrorWarningFill } from "react-icons/ri";
-
-const Section = styled.section`
-  background: #fff;
-  max-width: 100%;
-  margin-top: 20px;
-  padding: 20px;
-  border-radius: 12px;
-  transition: 0.5s;
-  position: relative;
-`;
-
-const TotalAmount = styled.div`
-  display: flex;
-  justify-content: space-between;
-`;
-
-const ClearButton = styled.button`
-  background: transparent;
-  color: red;
-  width: 120px;
-  border: 2px solid red;
-  border-radius: 5px;
-  padding: 5px 10px;
-  font-size: 1.2rem;
-  cursor: pointer;
-  transition: all 0.1s ease-in-out;
-
-  &:hover {
-    background: red;
-    color: #fff;
-  }
-`;
-
-const Total = styled.p`
-  margin-top: 10px;
-  font-size: 1.4rem;
-  text-align: right;
-`;
-
-const WarningContainer = styled.div`
-  background: #feebc8;
-  width: 100%;
-  display: flex;
-  padding: 15px;
-`;
-
-const WarningText = styled.p`
-  font-size: 1.4rem;
-
-  @media (max-width: 379px) {
-    font-size: 5.5vw;
-  }
-`;
-
-const WarningIcon = styled(RiErrorWarningFill)`
-  color: #dd6b20;
-  margin-right: 10px;
-`;
+import {
+  Section,
+  TotalAmount,
+  ClearButton,
+  Total,
+  WarningContainer,
+  WarningText,
+  WarningIcon,
+} from "./styles";
 
 function Basket() {
   const dispatch = useDispatch();
---
+++ src/pages/Basket/styles.jsx
@@ -0,0 +1,60 @@
+import styled from "styled-components";
+import { RiErrorWarningFill } from "react-icons/ri";
+
+export const Section = styled.section`
+  background: #fff;
+  max-width: 100%;
+  margin-top: 20px;
+  padding: 20px;
+  border-radius: 12px;
+  transition: 0.5s;
+  position: relative;
+`;
+
+export const TotalAmount = styled.div`
+  display: flex;
+  justify-content: space-between;
+`;
+
+export const ClearButton = styled.button`
+  background: transparent;
+  color: red;
+  width: 120px;
+  border: 2px solid red;
+  border-radius: 5px;
+  padding: 5px 10px;
+  font-size: 1.2rem;
+  cursor: pointer;
+  transition: all 0.1s ease-in-out;
+
+  &:hover {
+    background: red;
+    color: #fff;
+  }
+`;
+
+export const Total = styled.p`
+  margin-top: 10px;
+  font-size: 1.4rem;
+  text-align: right;
+`;
+
+export const WarningContainer = styled.div`
+  background: #feebc8;
+  width: 100%;
+  display: flex;
+  padding: 15px;
+`;
+
+export const WarningText = styled.p`
+  font-size: 1.4rem;
+
+  @media (max-width: 379px) {
+    font-size: 5.5vw;
+  }
+`;
+
+export const WarningIcon = styled(RiErrorWarningFill)`
+  color: #dd6b20;
+  margin-right: 10px;
+`;
