2b5ddf5a6a86d070e0b833f567e5c3b4d5482244
Neşathan Öztürk <nesathanozturk@Nesathan-MacBook-Pro.local>
2023-02-20 12:51:15 +0300

    style: responsive 404 page

--- src/pages/NotFound404/styles.jsx
+++ src/pages/NotFound404/styles.jsx
@@ -5,34 +5,54 @@ export const Section = styled.section`
   height: 100vh;
   background: #f0f0f0;
   text-align: center;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
 `;
 
 export const Image = styled.img`
   display: block;
-  margin: 0 auto;
+
+  @media (max-width: 669px) {
+    width: 100%;
+  }
 `;
 
 export const ErrorText = styled.h1`
   padding: 20px;
+
+  @media (max-width: 768px) {
+    font-size: 1.3rem;
+  }
 `;
 
 export const Text = styled.p`
   margin-bottom: 40px;
   font-size: 20px;
+
+  @media (max-width: 768px) {
+    font-size: 18px;
+  }
 `;
 
 export const Button = styled.div`
   .error {
     background: #f0a04b;
     color: #fff;
-    padding: 12px;
-    font-size: 1.5rem;
+    padding: 11px;
+    font-size: 1.3rem;
     text-decoration: none;
     border-radius: 10px;
     transition: background 0.3s ease-in-out;
 
     &:hover {
       background: #d3883d;
+    }
+
+    @media (max-width: 768px) {
+      padding: 10px;
+      font-size: 1.2rem;
     }
   }
 `;
