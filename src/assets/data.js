4bf24f504ba113a5ef7f1e66248e6e0eba8faae8
Neşathan Öztürk <nesathanozturk@Nesathan-MacBook-Pro.local>
2023-02-20 15:50:43 +0300

    feat: added images

--- src/assets/data.js
+++ src/assets/data.js
@@ -1,31 +1,44 @@
+import bg1 from "./images/bg-1.jpg";
+import bg2 from "./images/bg-2.jpg";
+import bg3 from "./images/bg-3.jpg";
+import bg4 from "./images/bg-4.avif";
+import lipstick from "./images/lipstick.avif";
+import whiteWatch from "./images/white-watch.avif";
+import raybanGlass from "./images/rayban-glass.avif";
+import blackTshirt from "./images/black-tshirt.avif";
+import outCast from "./images/out-cast.avif";
+import grayJacket from "./images/gray-jacket.avif";
+import blueJacket from "./images/blue-jacket.avif";
+import brownCoat from "./images/brown-coat.avif";
+import whiteTshirt from "./images/white-tshirt.avif";
+import blackTshirt2 from "./images/black-tshirt2.avif";
+import whiteShirt from "./images/white-shirt.avif";
+import blackShirt from "./images/black-shirt.avif";
+import graySuit from "./images/gray-suit.avif";
+import chuck70 from "./images/chuck-70.avif";
+import adidas from "./images/adidas.avif";
+import nike from "./images/nike-air.avif";
+import blueSuit from "./images/blue-suit.avif";
+
 export const images = [
   {
     id: 1,
-    image:
-      "https://www.mrporter.com/cms/ycm/resource/blob/516956/afc2ed4911afe2c5b6ad4fee04381a84/9562a843-0ebd-4009-8a19-31fa2192dd87-data.jpg",
+    image: bg1,
     alt: "Background Poster",
   },
   {
     id: 2,
-    image:
-      "https://rare-gallery.com/uploads/posts/343957-Kendall-Jenner-American-Fashion-Model-Celebrity-Girls-Women-Smile.jpg",
+    image: bg2,
     alt: "Background Poster",
   },
   {
     id: 3,
-    image: "https://wallpaperaccess.com/full/1438079.jpg",
+    image: bg3,
     alt: "Background Poster",
   },
   {
     id: 4,
-    image:
-      "https://img.freepik.com/free-photo/high-fashion-portrait-young-elegant-blonde-woman-black-wool-hat-wearing-oversize-white-fringe-poncho-with-long-grey-dress_273443-3799.jpg?w=1380&t=st=1676744891~exp=1676745491~hmac=b47f0e0abaca262157801c5bc1be6605b48a3a3e4871d69253a32df2ae6e5ef5",
-    alt: "Background Poster",
-  },
-  {
-    id: 5,
-    image:
-      "https://img.freepik.com/free-photo/stylish-short-haired-woman-casual-leather-coat-jeans-posing-urban-brick-wall_273443-4559.jpg?w=1380&t=st=1676745134~exp=1676745734~hmac=b34c9af8d1ce3b9cc550265a46ff8ff322747e8c0032d78b38d8b827fb10137d",
+    image: bg4,
     alt: "Background Poster",
   },
 ];
@@ -34,16 +47,14 @@ export const products = [
   {
     id: 1,
     title: "Red Lipstick",
-    image:
-      "https://images.unsplash.com/photo-1586495777744-4413f21062fa?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=715&q=80",
+    image: lipstick,
     price: 20,
     amount: 1,
   },
   {
     id: 2,
     title: "White Watch",
-    image:
-      "https://images.unsplash.com/photo-1523275335684-37898b6baf30?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1399&q=80",
+    image: whiteWatch,
     price: 50,
     salePrice: 40,
     amount: 1,
@@ -51,8 +62,7 @@ export const products = [
   {
     id: 3,
     title: "Rayban Glass",
-    image:
-      "https://images.unsplash.com/photo-1572635196237-14b3f281503f?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=880&q=80",
+    image: raybanGlass,
     price: 100,
     salePrice: 60,
     amount: 1,
@@ -60,8 +70,7 @@ export const products = [
   {
     id: 4,
     title: "Black T-Shirt",
-    image:
-      "https://images.unsplash.com/photo-1503341504253-dff4815485f1?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80",
+    image: blackTshirt,
     price: 30,
     salePrice: 10,
     amount: 1,
@@ -69,104 +78,91 @@ export const products = [
   {
     id: 5,
     title: "Out Cast T-Shirt",
-    image:
-      "https://images.unsplash.com/photo-1527719327859-c6ce80353573?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=764&q=80",
+    image: outCast,
     price: 40,
     amount: 1,
   },
   {
     id: 6,
     title: "Gray Jacket",
-    image:
-      "https://images.unsplash.com/photo-1580742471944-c1c187a943e2?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=668&q=80",
+    image: grayJacket,
     price: 60,
     amount: 1,
   },
   {
     id: 7,
     title: "Blue Jacket",
-    image:
-      "https://images.unsplash.com/photo-1551537482-f2075a1d41f2?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80",
+    image: blueJacket,
     price: 50,
     amount: 1,
   },
   {
     id: 8,
     title: "Brown Coat",
-    image:
-      "https://images.unsplash.com/photo-1608063615781-e2ef8c73d114?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80",
+    image: brownCoat,
     price: 80,
     amount: 1,
   },
   {
     id: 9,
     title: "White T-Shirt",
-    image:
-      "https://images.unsplash.com/photo-1627910087352-f4e3e28d9f5f?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80",
+    image: whiteTshirt,
     price: 20,
     amount: 1,
   },
   {
     id: 10,
     title: "Black T-Shirt",
-    image:
-      "https://images.unsplash.com/photo-1627910016961-ee310ef0b108?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=627&q=80",
+    image: blackTshirt2,
     price: 15,
     amount: 1,
   },
   {
     id: 11,
     title: "White Shirt",
-    image:
-      "https://images.unsplash.com/photo-1621072156002-e2fccdc0b176?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80",
+    image: whiteShirt,
     price: 45,
     amount: 1,
   },
   {
     id: 12,
     title: "Black Shirt",
-    image:
-      "https://images.unsplash.com/photo-1624824216985-5639b3071ce6?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1yZWxhdGVkfDZ8fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=800&q=60",
+    image: blackShirt,
     price: 42,
     amount: 1,
   },
   {
     id: 13,
     title: "Gray Suit",
-    image:
-      "https://images.unsplash.com/photo-1619102814948-e164e584cf0c?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80",
+    image: graySuit,
     price: 150,
     amount: 1,
   },
   {
     id: 14,
     title: "Chuck 70",
-    image:
-      "https://images.unsplash.com/photo-1463100099107-aa0980c362e6?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80",
+    image: chuck70,
     price: 100,
     amount: 1,
   },
   {
     id: 15,
     title: "Adidas",
-    image:
-      "https://images.unsplash.com/flagged/photo-1556637640-2c80d3201be8?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80",
+    image: adidas,
     price: 120,
     amount: 1,
   },
   {
     id: 16,
     title: "Nike",
-    image:
-      "https://images.unsplash.com/photo-1516478177764-9fe5bd7e9717?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80",
+    image: nike,
     price: 130,
     amount: 1,
   },
   {
     id: 17,
     title: "Blue Suit",
-    image:
-      "https://images.unsplash.com/photo-1593030103066-0093718efeb9?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1160&q=80",
+    image: blueSuit,
     price: 180,
     amount: 1,
   },
